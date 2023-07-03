092ac3d70841312bfab8236372523a4789c39fc2
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2023-07-03 20:06:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+// Aggiungi un listener per il click sul bottone "Send"
+document.getElementById('send-button').addEventListener('click', function () {
+    // Ottieni il valore del messaggio dall'input
+    var messageInput = document.getElementById('message-input');
+    var messageContent = messageInput.value.trim();
+
+    // Verifica se il messaggio non è vuoto
+    if (messageContent !== '') {
+        // Crea gli elementi HTML per visualizzare il messaggio inviato dall'utente
+        var messageContainer = document.createElement('div');
+        messageContainer.className = 'message';
+
+        var messageSender = document.createElement('div');
+        messageSender.className = 'message-sender';
+        messageSender.textContent = 'You';
+
+        var messageContentElement = document.createElement('div');
+        messageContentElement.className = 'message-content';
+        messageContentElement.textContent = messageContent;
+
+        // Aggiungi gli elementi del messaggio inviato dall'utente al container del messaggio
+        messageContainer.appendChild(messageSender);
+        messageContainer.appendChild(messageContentElement);
+
+        // Ottieni il corpo della chat e aggiungi il messaggio inviato dall'utente al suo interno
+        var chatBody = document.querySelector('.chat-body');
+        chatBody.appendChild(messageContainer);
+
+        // Aggiungi la risposta dell'assistente virtuale dopo un breve ritardo
+        setTimeout(function () {
+            // Crea gli elementi HTML per visualizzare la risposta dell'assistente virtuale
+            var assistantResponseContainer = document.createElement('div');
+            assistantResponseContainer.className = 'message';
+
+            var assistantResponseSender = document.createElement('div');
+            assistantResponseSender.className = 'message-sender';
+            assistantResponseSender.textContent = 'Assistente Virtuale';
+
+            var assistantResponseContent = document.createElement('div');
+            assistantResponseContent.className = 'message-content';
+            assistantResponseContent.textContent = getAssistantResponse(messageContent);
+
+            // Aggiungi gli elementi della risposta dell'assistente virtuale al container del messaggio
+            assistantResponseContainer.appendChild(assistantResponseSender);
+            assistantResponseContainer.appendChild(assistantResponseContent);
+
+            // Aggiungi il messaggio di risposta dell'assistente virtuale al corpo della chat
+            chatBody.appendChild(assistantResponseContainer);
+        }, 500); // Ritardo di 500 millisecondi (0.5 secondi) per simulare la risposta dell'assistente virtuale
+
+        // Resetta l'input del messaggio
+        messageInput.value = '';
+    }
+});
+
+// Funzione per ottenere la risposta dell'assistente virtuale in base al messaggio dell'utente
+function getAssistantResponse(message) {
+    var response = '';
+
+    // Logica di risposta personalizzata dell'assistente virtuale
+    if (message.toLowerCase().includes('ciao')) {
+        response = 'Ciao! Come posso aiutarti oggi?';
+    } else if (message.toLowerCase().includes('ordin') || message.toLowerCase().includes('prenot')) {
+        response = 'Accedi alla sezione Menù per ordinare il tuo piatto!';
+    } else if (message.toLowerCase().includes('apert')) {
+        response = 'Siamo sempre aperti, 24 ore no stop!';
+    } else if (message.toLowerCase().includes('dove') || message.toLowerCase().includes('indirizzo') || message.toLowerCase().includes('via')) {
+        response = 'Ci troviamo in via Growell!';
+    } else if (message.toLowerCase().includes('ore') || message.toLowerCase().includes('ora')) {
+        var date = new Date();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        response = 'L\'orario corrente è ' + hours + ':' + minutes + '.';
+    } else {
+        response = 'Mi scuso, non ho capito. Potresti ripetere, per favore?';
+    }
+
+    // Restituisci la risposta dell'assistente virtuale
+    return response;
+}
